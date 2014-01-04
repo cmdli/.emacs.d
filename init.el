59b37c9a7cba432de6e517f6adeb647253be3b5f
Chris de la Iglesia <Chris de la Iglesia>
2014-01-03 19:21:54 -0600

    Added backup file control to init.el (saves them in ~/.emacs-backups)

--- init.el
+++ init.el
@@ -1,7 +1,17 @@
 (require 'package)
 (add-to-list 'load-path "~/Dropbox/emacs")
 (load "globalemacs")
-(set-default-font "Source Code Pro-11")
+;;(set-default-font "Source Code Pro-11")
+
+(setq-default indent-tabs-mode nil)
+(setq-default c-basic-offset 4)
+
+(setq backup-directory-alist `(("." . "~/.emacs-backups")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)

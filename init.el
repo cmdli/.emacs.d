c77803aefc3674ba432e131f9e03d1e672de11c8
Chris de la Iglesia <cmdli@utexas.edu>
2014-12-12 21:07:46 -0600

    Added automatic package installation

--- init.el
+++ init.el
@@ -18,21 +18,25 @@
       kept-old-versions 2
       version-control t)
 
+(setq package-list '(solarized-theme ibuffer graphene scala-mode2))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
 (load-theme 'solarized-dark t)
-(require 'uniquify)
+;(require 'uniquify)
 (show-paren-mode 1)
 (setq show-paren-delay 0)
 (transient-mark-mode 1)
-(require 'ibuffer)
+;(require 'ibuffer)
 ;(blink-cursor-mode 1)
 (line-number-mode 1)
 (global-linum-mode 1)
-(require 'graphene)
-
-(unless (package-installed-p 'scala-mode2)
-  (package-refresh-contents) (package-install 'scala-mode2))
-(unless (package-installed-p 'graphene)
-  (package-refresh-contents) (package-install 'graphene))
+;(require 'graphene)
 
 (setq initial-scratch-message "")
 (setq inhibit-startup-message t)

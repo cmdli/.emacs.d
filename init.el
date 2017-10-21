9dd361cdd690c9e70f2146104104fea5dada5e77
Chris de la Iglesia <christopher.delaiglesia@gmail.com>
2017-10-20 21:30:17 -0700

    Fixed package installation and added groovy-mode

--- init.el
+++ init.el
@@ -2,8 +2,16 @@
 
 ;; Init package system
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+;; Download packages
+(setq package-list '(solarized-theme gradle-mode scala-mode groovy-mode))
+(unless package-archive-contents
+  (package-refresh-contents))
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 ;; Change tabs to spaces
 (setq-default indent-tabs-mode nil)
@@ -18,15 +26,6 @@
       kept-old-versions 2
       version-control t)
 
-;; Download packages
-(setq package-list '(solarized-theme scala-mode2))
-(unless package-archive-contents
-  (package-refresh-contents))
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-
 ;; Use packages
 (load-theme 'solarized-dark t)
 ;(require 'uniquify)
@@ -37,6 +36,7 @@
 ;(blink-cursor-mode 1)
 (line-number-mode 1)
 (global-linum-mode 1)
+(require 'gradle-mode)
 
 ;; Scratch buffer on startup
 (setq initial-scratch-message "")
@@ -51,3 +51,17 @@
 ;; Turn off tool bars and menus
 (tool-bar-mode 0)
 
+;; Start emacs server
+(server-start)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (scala-mode solarized-theme gradle-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
